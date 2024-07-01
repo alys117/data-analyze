a7197e8bc8c65216e8ab9f573b93f8bf23c084fa
keke <alys117@163.com>
2024-07-02 07:38:56 +0800

    合并行列demo

--- src/views/step-3.vue
+++ src/views/step-3.vue
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router'
 import { ref, onMounted } from 'vue'
 import DragDemo from '@/views/drag-demo.vue'
 import XlsxDemo from '@/views/xlsx-demo.vue'
+import SpanDemo from '@/views/span-demo.vue'
 const router = useRouter()
 
 const loading = ref(true)
@@ -19,6 +20,7 @@ onMounted(() => {
     </div>
     <drag-demo/>
     <xlsx-demo/>
+    <span-demo/>
     <div class="step-forward">
       <el-button size="default" type="primary" @click="router.push('/step2')">上一步</el-button>
       <el-button size="default" type="primary" @click="router.push('/step4')">下一步</el-button>
